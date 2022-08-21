c42f7f8d826ee640e2e4fdd46019bd7e40d55678
deepshikha-s <i.deepshikha.sinha@gmail.com>
2022-08-21 12:15:36 +0530

    Documentation of machine-learning-client.lua

--- plugin/machine-learning-client.lua
+++ plugin/machine-learning-client.lua
@@ -2,42 +2,43 @@
 -- and seek ml_inbound_status using such models
 -- this ml driver is invoked by machine-learning-plugin-after.conf
 -- currently most of this code is from https://github.com/coreruleset/coreruleset/pull/2067/files
--- set your machine learning server URL
+
+-- Variable Declarations:
+-- setting the machine learning server URL
 local ml_server_url = 'http://127.0.0.1:5000/'
--- local variable for inbound_ml_status update. By default it must be zero 
+-- initialising the variable to return the machine learning pass or block status
 local inbound_ml_result = 0
+
+-- Importing libraries
 local ltn12 = require("ltn12")
 local http = require("socket.http")
-local respbody = {}
 
 function main()
+  -- Initialising variables
   local method = m.getvar("REQUEST_METHOD")
   local path = m.getvar("REQUEST_FILENAME")
   local hour = m.getvar("TIME_HOUR")
   local day = m.getvar("TIME_DAY")
   local args = m.getvars("ARGS")
-  local args_names = m.getvars("ARGS_NAMES")
-  local argsnamesstr = "{}"
-  local args_str = "{}"
   local reqbody = m.getvars("REQUEST_BODY")
-  local reqbodystr = ""
-  local req_data = {}
-  local headersstr = ""
+  local args_names = m.getvars("ARGS_NAMES")
   local request_header = m.getvars("REQUEST_HEADERS")
   local files = m.getvars("FILES")
-  local filesstr = "{}"
   local filesizes = m.getvars("FILES_SIZES")
   local filenames = m.getvars("FILES_NAMES")
+  local argsnamesstr = "{}"
+  local args_str = "{}"
+  local reqbodystr = ""
+  local headersstr = "{}"
+  local filesstr = "{}"
   local filesizestr = "{}"
   local filenamestr = "{}"
-  local multi = m.getvar("FULL_REQUEST") 
-  local content_type = " " --request_header["REQUEST_HEADERS:Content-Type"]
+  local content_type = " " 
   local content_length = " "
   local headers = {}
+  local req_data = {}
+  local respbody = {}
   local body = " "
-  local processor = m.getvar("REQBODY_PROCESSOR")
-  m.log(1, "processor "..processor)
-  m.log(1, "Content Type "..content_type)
   req_data["req_protocol"] = m.getvar("REQUEST_PROTOCOL")
   req_data["req_uri"] = m.getvar("REQUEST_URI")
   req_data["req_method"] = m.getvar("REQUEST_METHOD")
@@ -45,6 +46,8 @@ function main()
   req_data["file_name"] = m.getvar("REQUEST_FILENAME")
   req_data["base_name"] = m.getvar("REQUEST_BASENAME")
   req_data["headers_name"] = m.getvar("REQUEST_HEADERS_NAMES")
+
+  -- Logging some variables
   m.log(4, "RequestProtocol: " ..req_data["req_protocol"])
   m.log(1, "RequestURI: " ..req_data["req_uri"])
   m.log(1, "RequestMethod: " ..req_data["req_method"])
@@ -52,151 +55,151 @@ function main()
   m.log(1, "RequestFilename: " ..req_data["file_name"])
   m.log(1, "RequestBasename: " ..req_data["base_name"])
   m.log(1, "RequestHeadersName: " ..req_data["headers_name"])
-  -- transform the args array into a string following JSON format
+
+  -- Parsing the tables and logging
   if request_header ~= nil then
-      headersstr = "{"
-      for k,v in pairs(request_header) do
-        name = v["name"]
-        value = v["value"]
-        m.log(1, "name "..name)
-        if name == "REQUEST_HEADERS:Content-Type" then
-          content_type = value
-        end
-        if name == "REQUEST_HEADERS:Content-Length" then
-          content_length = value
-        end
-        value = value:gsub('"', "$#$")
-        headersstr = headersstr..'"'..name..'":"'..value..'",'
+    headersstr = "{"
+    for k,v in pairs(request_header) do
+      name = v["name"]
+      value = v["value"]
+      m.log(1, "name "..name)
+      if name == "REQUEST_HEADERS:Content-Type" then
+        content_type = value
       end
-      if #request_header == 0 then
-        headersstr = "{}"
-      else
-        headersstr = string.sub(headersstr, 1, -2)
-        headersstr = headersstr.."}"
+      if name == "REQUEST_HEADERS:Content-Length" then
+        content_length = value
       end
+      value = value:gsub('"', "$#$")
+      headersstr = headersstr..'"'..name..'":"'..value..'",'
     end
+    if #request_header == 0 then
+      headersstr = "{}"
+    else
+      headersstr = string.sub(headersstr, 1, -2)
+      headersstr = headersstr.."}"
+    end
+  end
   m.log(1, "Header "..headersstr)
+
   if reqbody ~= nil then
-      reqbodystr = "{"
-      for k,v in pairs(reqbody) do
-        name = v["name"]
-        value = v["value"]
-        value = value:gsub('"', "$#$")
-        reqbodystr = reqbodystr..'"'..name..'":"'..value..'",'
-      end
-      if #reqbody == 0 then
-        reqbodystr = "{}"
-      else
-        reqbodystr = string.sub(reqbodystr, 1, -2)
-        reqbodystr = reqbodystr.."}"
-      end
+    reqbodystr = "{"
+    for k,v in pairs(reqbody) do
+      name = v["name"]
+      value = v["value"]
+      value = value:gsub('"', "$#$")
+      reqbodystr = reqbodystr..'"'..name..'":"'..value..'",'
     end
+    if #reqbody == 0 then
+      reqbodystr = "{}"
+    else
+      reqbodystr = string.sub(reqbodystr, 1, -2)
+      reqbodystr = reqbodystr.."}"
+    end
+  end
   m.log(1, "BODY "..reqbodystr)
-  --function json()
-    if args ~= nil then
-      args_str = "{"
-      for k,v in pairs(args) do
-        name = v["name"]
-        value = v["value"]
-        value = value:gsub('"', "$#$")
-        args_str = args_str..'"'..name..'":"'..value..'",'
-      end
-      if #args == 0 then
-        args_str = "{}"
-      else
-        args_str = string.sub(args_str, 1, -2)
-        args_str = args_str.."}"
-      end
+
+  if args ~= nil then
+    args_str = "{"
+    for k,v in pairs(args) do
+      name = v["name"]
+      value = v["value"]
+      value = value:gsub('"', "$#$")
+      args_str = args_str..'"'..name..'":"'..value..'",'
     end
-  --end
+    if #args == 0 then
+      args_str = "{}"
+    else
+      args_str = string.sub(args_str, 1, -2)
+      args_str = args_str.."}"
+    end
+  end
   m.log(1, "Args "..args_str)
+
   if args_names ~= nil then
-      argsnamesstr = "{"
-      for k,v in pairs(args_names) do
-        name = v["name"]
-        value = v["value"]
-        value = value:gsub('"', "$#$")
-        argsnamesstr = argsnamesstr..'"'..name..'":"'..value..'",'
-      end
-      if #args_names == 0 then
-        argsnamesstr = "{}"
-      else
-        argsnamesstr = string.sub(argsnamesstr, 1, -2)
-        argsnamesstr = argsnamesstr.."}"
-      end
+    argsnamesstr = "{"
+    for k,v in pairs(args_names) do
+      name = v["name"]
+      value = v["value"]
+      value = value:gsub('"', "$#$")
+      argsnamesstr = argsnamesstr..'"'..name..'":"'..value..'",'
     end
-  --end
+    if #args_names == 0 then
+      argsnamesstr = "{}"
+    else
+      argsnamesstr = string.sub(argsnamesstr, 1, -2)
+      argsnamesstr = argsnamesstr.."}"
+    end
+  end
   m.log(1, "Args Names "..argsnamesstr)
+
   if files ~= nil then
-      filesstr = "{"
-      for k,v in pairs(files) do
-        name = v["name"]
-        value = v["value"]
-        value = value:gsub('"', "$#$")
-        filesstr = filesstr..'"'..name..'":"'..value..'",'
-      end
-      if #files == 0 then
-        filesstr = "{}"
-      else
-        filesstr = string.sub(filesstr, 1, -2)
-        filesstr = filesstr.."}"
-      end
+    filesstr = "{"
+    for k,v in pairs(files) do
+      name = v["name"]
+      value = v["value"]
+      value = value:gsub('"', "$#$")
+      filesstr = filesstr..'"'..name..'":"'..value..'",'
     end
-  --end
+    if #files == 0 then
+      filesstr = "{}"
+    else
+      filesstr = string.sub(filesstr, 1, -2)
+      filesstr = filesstr.."}"
+    end
+  end
   m.log(1, "Files "..filesstr)
-if filenames ~= nil then
-      filenamestr = "{"
-      for k,v in pairs(filenames) do
-        name = v["name"]
-        value = v["value"]
-        value = value:gsub('"', "$#$")
-        filenamestr = filenamestr..'"'..name..'":"'..value..'",'
-      end
-      if #filenames == 0 then
-        filenamestr = "{}"
-      else
-        filenamestr = string.sub(filenamestr, 1, -2)
-        filenamestr = filenamestr.."}"
-      end
+
+  if filenames ~= nil then
+    filenamestr = "{"
+    for k,v in pairs(filenames) do
+      name = v["name"]
+      value = v["value"]
+      value = value:gsub('"', "$#$")
+      filenamestr = filenamestr..'"'..name..'":"'..value..'",'
     end
-  --end
+    if #filenames == 0 then
+      filenamestr = "{}"
+    else
+      filenamestr = string.sub(filenamestr, 1, -2)
+      filenamestr = filenamestr.."}"
+    end
+  end
   m.log(1, "Files Names"..filenamestr)
-if filesizes ~= nil then
-      filesizestr = "{"
-      for k,v in pairs(filesizes) do
-        name = v["name"]
-        value = v["value"]
-        value = value:gsub('"', "$#$")
-        filesizestr = filesizestr..'"'..name..'":"'..value..'",'
-      end
-      if #filesizes == 0 then
-        filesizestr = "{}"
-      else
-        filesizestr = string.sub(filesizestr, 1, -2)
-        filesizestr = filesizestr.."}"
-      end
+
+  if filesizes ~= nil then
+    filesizestr = "{"
+    for k,v in pairs(filesizes) do
+      name = v["name"]
+      value = v["value"]
+      value = value:gsub('"', "$#$")
+      filesizestr = filesizestr..'"'..name..'":"'..value..'",'
     end
-  --end
+    if #filesizes == 0 then
+      filesizestr = "{}"
+    else
+      filesizestr = string.sub(filesizestr, 1, -2)
+      filesizestr = filesizestr.."}"
+    end
+  end
   m.log(1, "File Sizes "..filesizestr)
   m.log(1, "Content "..content_type.." "..content_length)
-  -- construct http request for the ml server
+
+  -- Construct http request for the ml server
   if content_type == " " then
     body = "method="..method.."&path="..path.."&args="..args_str.."&hour="..hour.."&day="..day
     headers = {
-      --["Content-Type"] = "multipart/form-data";
       ["Content-Type"] = "application/x-www-form-urlencoded";
       ["Content-Length"] = #body
     }
   else
     body = "method="..method.."&path="..path.."&args="..args_str.."&files="..filesstr.."&hour="..hour.."&day="..day
-    --body = "method="..method.."&path="..path.."&args="..args_str.."&hour="..hour.."&day="..day
     headers = {
-      --["Content-Type"] = content_type;
       ["Content-Type"] = "application/x-www-form-urlencoded";
       ["Content-Length"] = #body
     }
     m.log(1, "REQ body "..body)
   end
+
   local source = ltn12.source.string(body)
   local client, code, headers, status = http.request{
     url=ml_server_url, 
@@ -206,6 +209,8 @@ if filesizes ~= nil then
     sink = ltn12.sink.table(respbody)
   }
   respbody = table.concat(respbody)
+
+-- Processing the result
   if client == nil then
     m.log(1, 'The server is unreachable \n')
   end
